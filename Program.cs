051775eb476d23450d1a805b0f8148cf06dbe1ec
Jamen <jamenw@gmail.com>
2022-03-09 21:52:01 -0700

    Part 3 C# Version 1

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+//using Newtonsoft.Json;
+using System.Text;
+
+namespace InterviewChallenge
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            using (var client = new HttpClient())
+            {
+                var info_challenge = new Uri("https://si-interview-api.azurewebsites.net/api/Information/Challenge");
+                var result1 = client.GetAsync(info_challenge).Result.Content.ReadAsStringAsync().Result;
+
+                var weather_forecast_get = new Uri("https://si-interview-api.azurewebsites.net/api/WeatherForecast/WeatherForecast");
+                var result2 = client.GetAsync(weather_forecast_get).Result.Content.ReadAsStringAsync().Result;
+
+                var weather_forecast_location = new Uri("https://si-interview-api.azurewebsites.net/api/WeatherForecast/WeatherForecast/salt%20lake%20city");
+                var result3 = client.GetAsync(weather_forecast_location).Result.Content.ReadAsStringAsync().Result;
+
+
+            }
+        }
+
+
+    }
+}
